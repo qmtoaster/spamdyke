357a01e598dba7507c8de46988b45438b0308523
root <root@pet110ii.whitehorsetc.com>
2016-11-16 13:15:05 -0700

    First Commit

---
+++ 4to5config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sed -i \
+-e 's/reject-missing-sender-mx/reject-sender=no-mx/g' \
+-e 's/rejection-text-missing-sender-mx/rejection-text-sender-no-mx/g' \
+-e 's/reject-identical-sender-recipient/reject-recipient=same-as-sender/g' \
+-e 's/rejection-text-identical-sender-recipient/rejection-text-recipient-same-as-sender/g' \
+-e 's/local-domains-file/qmail-rcpthosts-file/g' \
+-e 's/local-domains-entry=/#local-domains-entry=(Add these entries to qmail-rcpthosts-file)/g' \
+-e 's/morercpthosts/qmail-morercpthosts-cdb/'g /etc/spamdyke/spamdyke.conf
